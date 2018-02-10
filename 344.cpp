f7c420870b19c0cbbb01d31d5b83f42dcb41bb2b
UserCr <92Sister@gmail.com>
2018-02-10 18:11:29 +0800

    Create 344.cpp

---
+++ 344.cpp
@@ -0,0 +1,15 @@
+/**
+水题。
+**/
+
+class Solution {
+public:
+    string reverseString(string s) {
+        for(int i = 0 , j = s.length() - 1; i < j; i++, j--) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
+        return s;
+    }
+};
