3ac519e50a6e290e422aa879da0f3f9790fae1f6
UserCr <92Sister@gmail.com>
2018-02-20 20:21:46 +0800

    Create 657.c

---
+++ 657.c
@@ -0,0 +1,26 @@
+/**
+水题。
+**/
+
+bool judgeCircle(char* moves) {
+    int x = 0, y = 0;
+    for(int i = 0; moves[i] != 0; ++i) {
+        switch(moves[i]) {
+            case 'U':
+                y++;
+                break;
+            case 'D':
+                y--;
+                break;
+            case 'L':
+                x--;
+                break;
+            case 'R':
+                x++;
+                break;
+            default:
+                break;
+        }
+    }
+    return x == 0 && y == 0;
+}
