369a44f8997fbe3ac1d07cf87e5439174219ad25
UserCr <92Sister@gmail.com>
2018-02-24 15:58:05 +0800

    Update 147.java

--- 147.java
+++ 147.java
@@ -1,5 +1,7 @@
 /**
-题目是插入排序，十分简单。
+插入排序，水题。
+**/
+
 class Solution {
     public ListNode insertionSortList(ListNode head) {
         if (null == head) return null;
@@ -23,58 +25,6 @@ class Solution {
                 before.next.next = next;
             }
         }
-
-        return result;
-    }
-}
-其实可以用归并排序，速度会快不少。
-**/
-class Solution {
-    public ListNode insertionSortList(ListNode head) {
-        if (null == head || null == head.next) return head;
-
-        ListNode fast = head, slow = head, prevSlow = head;
-        while (null != fast && null != fast.next) {
-            prevSlow = slow;
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        prevSlow.next = null;
-
-        ListNode list1 = insertionSortList(head);
-        ListNode list2 = insertionSortList(slow);
-
-        if (null == list1) return list2;
-        if (null == list2) return list1;
-
-        ListNode result = null;
-        ListNode resultEnd = null;
-
-        while (null != list1 && null != list2) {
-            if (list1.val < list2.val) {
-                if (null == result) {
-                    result = list1;
-                    resultEnd = result;
-                } else {
-                    resultEnd.next = list1;
-                    resultEnd = resultEnd.next;
-                }
-                list1 = list1.next;
-            } else {
-                if (null == result) {
-                    result = list2;
-                    resultEnd = result;
-                } else {
-                    resultEnd.next = list2;
-                    resultEnd = resultEnd.next;
-                }
-                list2 = list2.next;
-            }
-        }
-
-        if (null != list1) resultEnd.next = list1;
-        if (null != list2) resultEnd.next = list2;
 
         return result;
     }
